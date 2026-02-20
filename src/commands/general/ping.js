e970a43ed1eb053fef8714499d6369e043ef22c6
xodo2fast4u <officialrtskhosana@gmail.com>
2026-02-20 13:45:38 +0200

    refactor(ping): remove latency logic to avoid duplication with speed command

--- src/commands/general/ping.js
+++ src/commands/general/ping.js
@@ -3,27 +3,9 @@ import { send } from "../../utils/utils.js";
 
 export default {
   cmd: ["ping", "p"],
-  desc: "Check bot latency",
+  desc: "Check if bot is alive",
 
   run: async (sock, msg) => {
-    const start = Date.now();
-    const sent = await send.text(sock, msg, `${e.ping} Pinging...`);
-    const ms = Date.now() - start;
-
-    const status =
-      ms < 100
-        ? `${e.sonic} Sonic Speed!`
-        : ms < 200
-          ? `${e.rocket} Fast!`
-          : ms < 500
-            ? `${e.bolt} Normal`
-            : "🐢 Slow";
-
-    await send.edit(
-      sock,
-      msg,
-      sent.key,
-      `${e.ping} *Pong!* ${ms}ms\n${status}`,
-    );
+    await send.text(sock, msg, `${e.ping} Pong!`);
   },
 };
